9c5f523c0a97aff68c9288cbb5c870791108d67b
Angeel22m <135563925+Angeel22m@users.noreply.github.com>
2023-06-27 17:10:09 -0600

    Add files via upload

---
+++ java/unidad8/ejercicio3/PruebaEmpleado.java
@@ -0,0 +1,21 @@
+package unidad8.ejercicio3;
+
+ class PruebaEmpleado {
+	
+	public static void main(String[] args) {
+		
+		Fecha nacimiento = new Fecha(12, 31, 1949);
+		Fecha contratacion = new Fecha(2, 29, 2020);
+		
+		Empleado empleado = new Empleado("Bob", "Blue", nacimiento, contratacion);
+		
+		System.out.println(empleado);
+		
+		nacimiento.incrementarDia();
+		System.out.println(empleado);
+		contratacion.incrementarDia();
+		System.out.println(empleado);
+		
+	}
+
+}
